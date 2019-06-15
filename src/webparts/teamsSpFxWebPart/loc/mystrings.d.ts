1d71ae17ee4d70747eadca6c0c12dff42903e42c
koskila <posti@koskila.net>
2019-06-15 12:57:27 -0500

    Teams SPFx tab sample (tutorial + materials for my session) initial commit.

---
+++ src/webparts/teamsSpFxWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITeamsSpFxWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TeamsSpFxWebPartWebPartStrings' {
+  const strings: ITeamsSpFxWebPartWebPartStrings;
+  export = strings;
+}
